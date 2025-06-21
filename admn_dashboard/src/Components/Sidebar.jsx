ad2332a6746ca9d4101932f4ae79a2bab95872c5
tndcoder <tharushinethma392@gmail.com>
2025-06-21 06:46:50 -0700

    update dashboard ui content

--- admn_dashboard/src/Components/Sidebar.jsx
+++ admn_dashboard/src/Components/Sidebar.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import {  Nav } from 'react-bootstrap';
+import {  NavLink } from 'react-router-dom';
+
 import { HouseFill, BoxFill, PeopleFill, Receipt, GraphUp, Power, Speedometer2, MegaphoneFill, GraphUpArrow } from 'react-bootstrap-icons';
 import './Sidebar.css';
 
 const Sidebar = () => {
   return (
-    <div className='main'>
+    
         
       <div className='sidebar'>
         <div className='sidebar-menu'>
-          <span className='d-name'><h1>Admin </h1></span>
+         <br></br> <h1 className='d-name'>Admin </h1><br></br>
+          <NavLink to="/" className="sidebar-link"><Speedometer2 className="me-2" />Dashboard</NavLink>
+          <NavLink to="/Users" className="sidebar-link"><PeopleFill className="me-2" />Users</NavLink>
+          <NavLink to="/profit-loss" className="sidebar-link"><GraphUpArrow className="me-2" />Profit & Loss</NavLink>
+          <NavLink to="/Promotions" className="sidebar-link"><MegaphoneFill className="me-2" />Promotions</NavLink>
+          <NavLink to="/Inventory" className="sidebar-link"><Receipt className="me-2" />Inventory Control</NavLink>
+
           
-          <Nav defaultActiveKey="/Dashboard" className="flex-column">
-            
-             <Nav.Link href="/Dashboard" className="sidebar-link"><Speedometer2 className="me-2" />Dashboard</Nav.Link>
-             <Nav.Link eventKey="link-1" className="sidebar-link"><PeopleFill className="me-2" />Users</Nav.Link>
-             <Nav.Link eventKey="link-2" className="sidebar-link"><GraphUpArrow className="me-2" />Profit & Loss</Nav.Link>
-             <Nav.Link eventKey="link-2" className="sidebar-link"><MegaphoneFill className="me-2" />Promotions</Nav.Link>
-             <Nav.Link eventKey="link-2" className="sidebar-link"><Receipt className="me-2" />Inventory Control</Nav.Link>
-           
-          </Nav>
          
         </div>
 
       
         <div className="sidebar-footer">
-            <Nav.Link href="/logout" className="sidebar-logout"><Power className="me-2" />Logout</Nav.Link>
+           <NavLink to="/logout" className="sidebar-logout"><Power className="me-2" />Logout</NavLink>
+
         </div>
       </div>
-    </div>
+   
   )
 }
 
